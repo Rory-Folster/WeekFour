faf9ce2d03ee5385ecb7e4c66ef2095110cbd59d
Rory-Folster <61396000+Rory-Folster@users.noreply.github.com>
2020-03-16 12:34:09 +1300

    else and if

---
+++ index.js
@@ -0,0 +1,38 @@
+let red = 0;
+let yellow = 2;
+let green = 1;
+let light = 1;
+
+// if (condition) {   
+//     // Do something 
+//   } else {   
+//     // Do some other thing 
+//   }
+
+  
+//   // Note: This example doesn't contain valid code yet 
+// if (light is red) {
+//     stop walking 
+//   } else {
+//     continue walking 
+//   }
+
+  
+//   if (light is red) { 
+//     // Stop walking 
+//   } else if (cars around) { 
+//     // Stop walking 
+//   } else if (yet another condition) { 
+//     // Do yet another thing 
+//   } else { 
+//     // Do the final thing 
+//   }
+
+  if (light === red) {
+      console.log("STOP")
+  }else if (light === green) {
+      console.log("GO")
+  } else {
+      console.log
+  }
+  
